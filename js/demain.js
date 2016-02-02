453bc0490ff3dce809191245b95a00c7053f5dee
Andréas Livet <andreas.livet@gmail.com>
2016-02-02 22:56:34 +0100

    Change css, html and jss to a more metro style application

--- js/demain.js
+++ js/demain.js
@@ -12,5 +12,22 @@ $(function(){
 		$('.carousel-inner').each(function(ind, el){
 			$(el).children().first().addClass('active');
 		});
+		//Enlarge the content
+		$('.collapse').on('show.bs.collapse', function(e){
+			var col = $(e.target).parent();
+			//Insure the content is at the begining
+			$(col).detach().prependTo('#accordion-content');
+
+			$(col).animate({width:"100%"}, {complete: function(){
+				$(col).removeClass('col-md-3');
+			}});
+		});
+		//Schrink the content
+		$('.collapse').on('hide.bs.collapse', function(e){
+			var col = $(e.target).parent();
+			$(col).animate({width:"25%"}, {complete: function(){
+				$(col).addClass('col-md-3');
+			}});
+		});
 	});
 });
