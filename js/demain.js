299925d74e947afd91fced59f2703162c93b8425
Andréas Livet <andreas.livet@gmail.com>
2016-02-09 11:17:31 +0100

    Divide the content in 3 columns and display it in a row

--- js/demain.js
+++ js/demain.js
@@ -4,39 +4,48 @@ $(function(){
 		$.each(data.sections, function(ind, section){
 			section.index = ind;
 		});
-		var template = $('#template').html();
-		Mustache.parse(template);   // optional, speeds up future uses
-  		var rendered = Mustache.render(template, data);
-		$('#accordion').html(rendered);
+
+		//Divide the sections in 3 columns
+		var nbItemsInCol = Math.floor(data.sections.length / 3);
+		for(var i = 0 ; i < 3 ; i++){
+			var template = $('#template').html();
+			Mustache.parse(template);   // optional, speeds up future uses
+			var col = {};
+			var last = i != 2 ? i*nbItemsInCol + nbItemsInCol : data.sections.length;
+			col.sections = data.sections.slice(i*nbItemsInCol, last);
+			var rendered = Mustache.render(template, col);
+			$('#accordion').append(rendered);
+		}
 
 		//Hide the carousel cause it cusumes a lot
 		$('.carousel').hide();
 		$('.carousel').carousel('pause');
-
+		//Set the active element otherwise carousel plugin crash
 		$('.carousel-inner').each(function(ind, el){
 			$(el).children().first().addClass('active');
 		});
 
+		var colToRattach = null;
+		var previousContent = null;
 		//Enlarge the content
 		$('.collapse').on('show.bs.collapse', function(e){
 			window.scrollTo(0, 0);
-			var col = $(e.target).parent();
+			if(previousContent != null){
+				previousContent.collapse('hide');
+			}
+			previousContent = $(e.target);
+			var section = $(e.target).parent();
+			colToRattach = section.parent();
 			//Insure the content is at the begining
-			$(col).detach().prependTo('#accordion');
-			$(col).find('.carousel').show();
-			console.log($(col).children('.carousel'));
-			$(col).animate({width:"100%"}, {complete: function(){
-				$(col).removeClass('col-md-3');
-			}});
+			$(section).detach().prependTo('#content');
+			$(section).find('.carousel').show();
 		});
 
 		//Schrink the content
 		$('.collapse').on('hide.bs.collapse', function(e){
-			var col = $(e.target).parent();
-			$(col).find('.carousel').hide();
-			$(col).animate({width:"25%"}, {complete: function(){
-				$(col).addClass('col-md-3');
-			}});
+			var section = $(e.target).parent();
+			$(section).find('.carousel').hide();
+			$(section).detach().prependTo(colToRattach);
 		});
 	});
 });
